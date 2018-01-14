8f8e1785165221775af7e5fa857652d13e013928
Kobus <kobus.haarhuis@hotmail.com>
2018-01-14 12:29:07 +0100

    Removing drunk code

--- src/components/BetterHangMan.js
+++ src/components/BetterHangMan.js
@@ -3,18 +3,6 @@ module.exports = class BetterHangMan
 
   constructor(tries = 7)
   {
-    this.tries = tries
-    this.word = this.setWord()
-    this.guessedLetters = []
-    this.correctLetters = []
-    this.notCorrectLetters = []
-    this.template = ''
-
-    for (let i = 0; i < this.word.length; i++)
-    {
-      this.template += '_'
-    }
-
   }
 
   setWord ()
@@ -24,19 +12,7 @@ module.exports = class BetterHangMan
 
   guessLetter(letter)
   {
-    let index = this.word.indexOf(letter)
-    if (index !== -1)
-    {
-      this.guessedLetters.push(letter)
-      this.correctLetters.push(letter)
-    }
-    else
-    {
-      this.guessedLetters.push(letter)
-      this.notCorrectLetters.push(letter)
-      this.tries--
-    }
-
+    
   }
 
 }
